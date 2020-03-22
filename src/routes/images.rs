4376e8a3b56ff6146f7eabc852e73a2148dca046
EgorRepnikov <egorepnikov@gmail.com>
2020-03-22 20:36:28 +0300

    Refactor images routes

--- src/routes/images.rs
+++ src/routes/images.rs
@@ -13,32 +13,25 @@ pub struct StoreRequest {
 }
 
 pub async fn get(params: Path<(String, String)>) -> Result<NamedFile, std::io::Error> {
-    let path = std::path::Path::new(&std::env::current_dir().unwrap())
-        .join(&CONFIG.storage_dir)
-        .join(&CONFIG.images_storage_dir)
-        .join(params.0.to_string())
-        .join(params.1.to_string());
-    Ok(NamedFile::open(path)?)
+    Ok(NamedFile::open(resolve_path(&params.0, &params.1))?)
 }
 
 pub async fn store(resource: Path<String>, image: Json<StoreRequest>) -> Result<String, std::io::Error> {
-    let path = std::path::Path::new(&std::env::current_dir().unwrap())
-        .join(&CONFIG.storage_dir)
-        .join(&CONFIG.images_storage_dir)
-        .join(resource.to_string())
-        .join(&image.name);
     let data = decode(&image.data).unwrap_or(vec!());
-    let mut file = std::fs::File::create(path)?;
+    let mut file = std::fs::File::create(resolve_path(&resource, &image.name))?;
     file.write_all(data.as_slice())?;
     Ok("Created".to_string())
 }
 
 pub async fn remove(params: Path<(String, String)>) -> Result<String, std::io::Error> {
-    let path = std::path::Path::new(&std::env::current_dir().unwrap())
+    std::fs::remove_file(resolve_path(&params.0, &params.1))?;
+    Ok("Deleted".to_string())
+}
+
+fn resolve_path(resource: &String, name: &String) -> std::path::PathBuf {
+    std::path::Path::new(&std::env::current_dir().unwrap())
         .join(&CONFIG.storage_dir)
         .join(&CONFIG.images_storage_dir)
-        .join(params.0.to_string())
-        .join(params.1.to_string());
-    std::fs::remove_file(path)?;
-    Ok("Deleted".to_string())
+        .join(resource.to_string())
+        .join(name.to_string())
 }
